5ba6539662cd74f310083816bab6de16c1ba595a
Marcos <marcosugaldemartinez@gmail.com>
2023-06-12 19:12:16 +0200

    added, frontend and link table

--- back/sql/creation.sql
+++ back/sql/creation.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS links;
 DROP TABLE IF EXISTS users;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -7,4 +8,14 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL,
     password TEXT NOT NULL
+);
+
+CREATE TABLE IF EXIST links (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    short_url TEXT NOT NULL UNIQUE, 
+    origin_url TEXT NOT NULL,
+    uses INTEGER DEFAULT 0,
+    creation_date DATE, DEFAULT NOW,
+    user_id INTEGER, 
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
