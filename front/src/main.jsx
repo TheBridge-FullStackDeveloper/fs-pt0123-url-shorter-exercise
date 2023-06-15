c07d6af435533ded859b6cf7d13156664c904f27
Marcos <perezocanamarcos@gmail.com>
2023-06-15 18:53:03 +0200

    second commit

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom'
+import { Route, Switch, Redirect, Link, useRoute } from "wouter";
+import Register from "./pages/Register"
+
+
+
+const CustomLink = ({ href, children }) => {
+
+  const [isActive] = useRoute(href)
+
+  return (
+    <Link {...{ href }}>
+      <a {...{ href }}>{children}</a>
+    </Link>
+  )
+}
+
+const Container = () => {
+  return (
+    <div>
+      <nav>
+        <CustomLink href="/register">Register</CustomLink>
+        <CustomLink href="/register">Register</CustomLink>
+        <CustomLink href="/register">Register</CustomLink>
+      </nav>
+      <Switch>
+       
+        <Route path="/register" component={Register} />
+        <Redirect to="/register" />
+      </Switch>
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
 )
---
+++ front/src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Register() {
+  return (
+    <div>
+      <h1>Create an account</h1>
+    </div>
+  );
+}
+
+export default Register;
