b3a697840422f36d6abfef7e1eda6e0a8fc7d510
Riftland <fran.mscr@gmail.com>
2023-06-18 05:41:25 +0200

    feat: implemented logout service

--- front/src/components/Modal/index.jsx
+++ front/src/components/Modal/index.jsx
@@ -1,8 +1,11 @@
+import { useAuth } from "../../hooks";
 import Styled from "../../styles";
 import Button from "../Button";
 import Title from "../Title";
 
 const Modal = ({ handleModal }) => {
+  const { logout } = useAuth();
+
   return (
     <Styled.Page vertical="center">
       <Title tag="h2" position="center">
@@ -10,7 +13,7 @@ const Modal = ({ handleModal }) => {
       </Title>
       <Title tag="h3">Are you sure?</Title>
       <Button onClick={() => handleModal(false)}>Nop</Button>
-      <p>Yeah</p>
+      <p onClick={logout}>Yeah</p>
     </Styled.Page>
   );
 };
