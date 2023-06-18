d6116d8ceee1241cbad04000e4d1008a746560d4
Riftland <fran.mscr@gmail.com>
2023-06-18 05:31:47 +0200

    feat: created links table

--- back/sql/creation.sql
+++ back/sql/creation.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS link;
 DROP TABLE IF EXISTS users;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -7,4 +8,16 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL,
     password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS links (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    short_url TEXT NOT NULL UNIQUE,
+    origin_url TEXT NOT NULL,
+    uses_by_creator INTEGER DEFAULT 0,
+    uses INTEGER DEFAULT 0,
+    creation_date DATE DEFAULT NOW(),
+    created_by uuid REFERENCES users
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
