e4384669e904011daf62e3f504607eaddfc7f817
Riftland <fran.mscr@gmail.com>
2023-06-18 05:37:08 +0200

    chore: added Query Provider

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,23 +1,27 @@
-import "./main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Redirect, Route, Switch } from "wouter";
+import { Redirect, Switch } from "wouter";
+import "./main.css";
 import Styled from "./styles";
 
+import Route from "./components/Route";
+import Provider from "./context/Provider";
 import Login from "./pages/Login";
 import Panel from "./pages/Panel";
 import Register from "./pages/Register";
 
 const Main = () => {
   return (
-    <Styled.Wrapper>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/" component={Panel} />
-        <Redirect to="/login" />
-      </Switch>
-    </Styled.Wrapper>
+    <Provider>
+      <Styled.Wrapper>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/" guarded={true} component={Panel} />
+          <Redirect to="/login" />
+        </Switch>
+      </Styled.Wrapper>
+    </Provider>
   );
 };
 
