036e5cdac583754d626b443f5cb71160d9b3cf26
Riftland <fran.mscr@gmail.com>
2023-06-18 05:31:07 +0200

    feat: added strict level in authorizer middleware

--- back/middlewares/authorizer.js
+++ back/middlewares/authorizer.js
@@ -1,12 +1,12 @@
 const { deserialize } = require('simple-stateless-auth-library')
 const errors = require('../misc/errors')
 
-module.exports = (req, res, next) => {
+module.exports = (strict = true) => (req, res, next) => {
     const payload = deserialize(req)
 
-    if(!payload) return next(errors[401])
+    if(strict && !payload) return next(errors[401])
 
-    res.locals = payload
+    res.locals.user = payload || {}
 
     next()
 }
