353f31fc3623f962f52d7c670f16780bf70fdac4
Riftland <fran.mscr@gmail.com>
2023-06-18 05:33:22 +0200

    feat: created models for url generator

--- back/middlewares/index.js
+++ back/middlewares/index.js
@@ -1,7 +1,9 @@
 const authorizer = require('./authorizer')
 const fieldsValidator = require('./fieldsValidator')
+const checkUrl = require('./checkUrl')
 
 module.exports = {
     authorizer,
     fieldsValidator,
+    checkUrl,
 }
